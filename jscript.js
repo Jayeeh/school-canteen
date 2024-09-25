f332fb68cbc5e38376e0228bcd1b6b0b354a239b
Jayeeh <erroljustinecasbadillo23@gmail.com>
2024-09-25 18:34:04 +0800

    Adding up a helpful comment as guide

--- jscript.js
+++ jscript.js
@@ -1,19 +1,26 @@
+// Define an object 'allergies' to track children with different food allergies
 const allergies = { 
-    nuts: {},
-    milk: {},
-    gluten: {},
+    nuts: {}, // Object to store children with nut allergies
+    milk: {}, // Object to store children with milk allergies
+    gluten: {}, // Object to store children with gluten allergies
+    // Method to calculate and log the total number of children with food restrictions
     getTotalNumber: function() {
       console.log(`There are ${this.nuts.children.length + this.milk.children.length + this.gluten.children.length} children with food restrictions.`);
     }
-  };
-  
-  allergies.nuts.children = ["Becca", "Max", "Phil"];
-  allergies.milk.children = ["Simon", "Ada", "Stacy"];
-  allergies.gluten.children = ["Paul", "Lisa", "Beth", "Alex"];
-  
-  console.log(allergies.milk.children);
-  allergies.nuts.total = allergies.nuts.children.length;
-  allergies.milk.total = allergies.milk.children.length;
-  allergies.gluten.total = allergies.gluten.children.length;
-  
-  allergies.getTotalNumber();
+};
+
+// Assign arrays of children's names to each allergy type
+allergies.nuts.children = ["Becca", "Max", "Phil"];
+allergies.milk.children = ["Simon", "Ada", "Stacy"];
+allergies.gluten.children = ["Paul", "Lisa", "Beth", "Alex"];
+
+// Log the children with milk allergies
+console.log(allergies.milk.children);
+
+// Calculate and store the total number of children for each allergy type
+allergies.nuts.total = allergies.nuts.children.length;
+allergies.milk.total = allergies.milk.children.length;
+allergies.gluten.total = allergies.gluten.children.length;
+
+// Call the method to log the total number of children with food restrictions
+allergies.getTotalNumber();
